bf7bb7dbe15c690f187242b5e12e2ca1d66833d1
Brian Qu <brianqjn@amazon.com>
2026-01-14 23:47:05 -0500

    feat: 14.Longest Common Prefix

--- Arrays&Hashing/14_longest_common_prefix.py
+++ Arrays&Hashing/14_longest_common_prefix.py
@@ -1,5 +1,9 @@
 class Solution:
     def longestCommonPrefix(self, strs: list[str]) -> str:
+        """
+        time: O(m * n), m is the lenth of list strs, n is the shortest length in strs
+        space: O(1)
+        """
         n = len(strs[0])
 
         for i in range(n):
@@ -8,3 +12,19 @@ class Solution:
                     return s[:i]
         
         return strs[0]
+    
+    def longestCommonPrefix2(self, strs: list[str]) -> str:
+        # 1. choose first element prefix
+        # 2. loop the strs list, compare the current str with the prefix
+        # 3. update the new prefix
+        # 4. after the loop, will get the longest common prefix
+        prefix = strs[0]
+
+        for s in strs[1:]:
+            n = len(prefix)
+            for i in range(n):
+                if i == len(s) or s[i] != prefix[i]:
+                    prefix = s[:i]
+                    break
+        
+        return prefix
