1054f6c467783caab530ea9dc4068ff0aad3440a
BrianQJN <qujianning0401@gmail.com>
2026-01-15 22:52:50 -0500

    feat: 49.Group Anagrams

--- Arrays&Hashing/49_group_anagrams.py
+++ Arrays&Hashing/49_group_anagrams.py
@@ -14,4 +14,18 @@ class Solution:
         return list(res.values())
     
     def groupAnagrams2(self, strs: list[str]) -> list[list[str]]:
+        """
+        Use ord(char) to get the unicode ASCII of the character
+        The key in the res dict is the frequency array of english letters.
+        The value is the original str arrays that have the same letter frequency.
+        time: O(m * n)
+        space: O(1) + key: max all strs are diff O(26 * m) = O(m) + values: m strs store into map, n is the longest length of strs O(m * n)
+        """
+        res = defaultdict(list)
+        for s in strs:
+            frequency = [0] * 26
+            for char in s:
+                frequency[ord(char) - ord('a')] += 1
+            res[tuple(frequency)].append(s)
         
+        return list(res.values())
